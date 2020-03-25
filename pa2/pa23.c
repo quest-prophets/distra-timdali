ee4f0e539a613a204b48f8cca83f3569f45949d9
timlathy <tim.labushev@gmail.com>
2020-03-25 19:55:41 +0300

    pa2: parse opts

--- pa2/pa23.c
+++ pa2/pa23.c
@@ -1,15 +1,76 @@
-#include "banking.h"
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void transfer(void * parent_data, local_id src, local_id dst,
-              balance_t amount)
-{
-    // student, please implement me
+#include "banking.h"
+#include "ipcext.h"
+#include "log.h"
+
+void transfer(void* parent_data, local_id src, local_id dst, balance_t amount) {
+  // student, please implement me
 }
 
-int main(int argc, char * argv[])
-{
-    //bank_robbery(parent_data);
-    //print_history(all);
+void client_process_do_work(IPCIO* ipcio, Message* buf) {
+  // bank_robbery(parent_data);
+  // print_history(all);
+}
 
-    return 0;
+void start(local_id num_children) {
+  IPCIO* ipcio = ipc_init(num_children);
+  if (ipcio == NULL)
+    log_panic(PARENT_ID, "failed to initialize IPC state");
+
+  Message buf;
+
+  log_init_events_log();
+  log_init_pipes_log();
+  ipc_iterate_pipes(ipcio, log_pipe);
+  log_close_pipes_log();
+
+  for (local_id i = 0; i < num_children; ++i) {
+    int fork_res = fork();
+    if (fork_res == 0) {
+      // inside child process
+      ipc_set_up_child(ipcio, i);
+      // ???
+      return;
+    } else if (fork_res == -1) {
+      log_panic(PARENT_ID, "failed to create child process");
+    }
+  }
+
+  ipc_set_up_parent(ipcio);
+  ipc_ext_await_all_started(ipcio, &buf);
+
+  client_process_do_work(ipcio, &buf);
+
+  ipc_ext_await_all_done(ipcio, &buf);
+
+  for (local_id i = 0; i < num_children; ++i)
+    wait(NULL);
+}
+
+int main(int argc, char** argv) {
+  local_id nchildren = 0;
+
+  char opt;
+  while ((opt = getopt(argc, argv, "p:")) != -1)
+    if (opt == 'p')
+      nchildren = strtoul(optarg, NULL, 10);
+
+  if (nchildren < 1 || nchildren > MAX_CHILDREN || optind + nchildren != argc) {
+    fprintf(stderr,
+            "Usage: %s -p N S...\n"
+            "where: N    is the number of child processes (1..%d)\n"
+            "       S... is a space-separated list of N integers denoting\n"
+            "            initial balance for each child process\n",
+            argv[0], MAX_CHILDREN);
+    return 1;
+  }
+
+  start(nchildren);
+  return 0;
 }
